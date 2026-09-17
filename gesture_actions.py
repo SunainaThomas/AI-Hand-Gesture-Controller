1ebd04498e4ce807e8b7985dee5dd3e217ecf20f
Sunaina Thomas <sunainathomasdelhi@gmail.com>
2026-09-17 17:57:39 +0530

    Implement gesture action handling

---
+++ gesture_actions.py
@@ -0,0 +1,12 @@
+def perform_action(gesture):
+    if gesture == "thumbs_up":
+        print("Volume Up")
+
+    elif gesture == "thumbs_down":
+        print("Volume Down")
+
+    elif gesture == "fist":
+        print("Pause")
+
+    elif gesture == "open_palm":
+        print("Play")
