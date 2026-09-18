1b3474b3eaa7b9695337e192b34df09220299b16
Sunaina Thomas <sunainathomasdelhi@gmail.com>
2026-09-18 13:00:24 +0530

    Add parameters to Hands initialization in main.py

--- main.py
+++ main.py
@@ -22,3 +22,8 @@ mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
 
 hands = mp_hands.Hands(
+    static_image_mode=False,
+    max_num_hands=1,
+    min_detection_confidence=DETECTION_CONFIDENCE,
+    min_tracking_confidence=TRACKING_CONFIDENCE
+)
